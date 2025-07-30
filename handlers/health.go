17b825621e3930a5e41fe6e54de350e405c45642
EwanGreer <ewanja.greer@gmail.com>
2025-07-30 19:38:39 +0100

    feat: add version to health endpoint

--- handlers/health.go
+++ handlers/health.go
@@ -1,11 +1,11 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func (h *Handler) HandleUp(c echo.Context) error {
-	return c.NoContent(http.StatusOK)
+	return c.JSON(200, map[string]any{
+		"version": h.Version,
+	})
 }
